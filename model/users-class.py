b38d0504c700bc42cabc99d1a561fe93a5999f32
Emmanuel <3ma195@gmail.com>
2024-06-04 15:52:11 -0400

    adding all classes files

---
+++ model/users-class.py
@@ -0,0 +1,8 @@
+class Users_id:
+
+    def __init__(self, email, first_name, last_name, password):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
