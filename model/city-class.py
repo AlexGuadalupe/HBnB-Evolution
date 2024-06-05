6b6b4a5404462b0c9990cf35281bb23e004d4f4f
Emmanuel <3ma195@gmail.com>
2024-06-04 22:09:18 -0400

    fixing city-class.py

--- model/city-class.py
+++ model/city-class.py
@@ -1,11 +1,14 @@
+import uuid
+from datetime import datetime
+
 class City:
     city_count = 0
 
     def __init__(self, name, country_id):
-        self.city_id = City.city_count + 1
+        self.city_id = uuid.uuid4()
         self.name = name
         self.country_id = country_id
-        City.city_count += 1
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-    def __repr__(self):
-        return f"City({self.city_id}, {self.name}, {self.country_id})"
+        City.city_count += 1
