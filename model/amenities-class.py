1b3bb3e3dab78394a1a0230f4f4e9b9bf2323fca
Emmanuel <3ma195@gmail.com>
2024-06-04 22:08:10 -0400

    fixing amenities-class.py

--- model/amenities-class.py
+++ model/amenities-class.py
@@ -1,11 +1,14 @@
-class Amenity:
-    amenity_count = 0
+import uuid
+from datetime import datetime
+
+class Amenities:
+    amenities_count = 0
 
     def __init__(self, name, description):
-        self.amenity_id = Amenity.amenity_count + 1
+        self.amenities_id = uuid.uuid4()
         self.name = name
         self.description = description
-        Amenity.amenity_count += 1
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-    def __repr__(self):
-        return f"Amenity({self.amenity_id}, {self.name}, {self.description})"
+        Amenities.amenities_count += 1
