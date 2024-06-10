0a2187d73015899e1725683be9bdbfcbc80253e6
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-10 17:39:30 -0400

    adding files

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from api.user_api import user_blueprint
+from api.amenities_api import amenities_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(user_blueprint)
+app.register_blueprint(amenities_blueprint)
 
 if __name__ == '__main__':
     app.run()
