b7060e73e6bca07278ea8612808d06194a0a7890
Emmanuel <3ma195@gmail.com>
2024-06-10 11:55:06 -0400

    fixing pushing/pulling problem

---
+++ api/cities_api.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+cities = []
+countries = []
+
+@app.route('/countries', methods=['GET'])
+def get_countries():
+
+    """Get all pre-loaded countries"""
+    return jsonify(countries), 200
+
+@app.route('/countries/{country_code}', methods=['GET'])
+def get_country_info(country_code):
+    """Get information of a specific country ."""
+    country = next((_country for _country in countries if _country["code"] == country_code), None)
+    if country is None:
+        return jsonify({"error": "Country not found."}), 404
+    return jsonify(country), 200
+
+@app.route('/countries/{country_code}', methods=['GET'])
+def get_country(country_code):
+    """Get a specific country."""
+    country = next((_country for _country in countries if _country["code"] == country_code), None)
+    if country is None:
+        return jsonify({"error": "Country not found."}), 404
+    return jsonify(country), 200
+
+@app.route('/countries/{country_code}/cities', methods=['GET'])
+def get_cities_of_country(country_code):
+    """Get all cities of a specific country."""
+    country = next((_country for _country in countries if _country["code"] == country_code), None)
+    if country is None:
+        return jsonify({"error": "Country not found."}), 404
+    return jsonify(country["cities"]), 200
+
+@app.route('/city', methods=['POST'])
+def create_city():
+    """Create a new city."""
+    data = request.json
+    req_city_info = [
+    {"name", "code"}
+    ]
+
+    for info in req_city_info:
+        if info not in data:
+            return jsonify({"message": "Missing information."}), 400
+
+    new_city = {"id":len(cities) + 1,
+    "name":data["name"],
+    "code":data["code"]
+    }
+
+    cities.append(new_city)
+    return jsonify({"message": "New city created with sucess."}), 201
+
+@app.route('/cities', methods=['GET'])
+def get_cities():
+    """Get all cities."""
+    return jsonify(cities), 200
+
+@app.route('/cities/{city_id}', methods=['GET'])
+def update_city(city_id):
+    """Update a city."""
+    data = request.json
+    city = next((_city for _city in cities if _city["id"] == city_id), None)
+    if city is None:
+        return jsonify({"error": "City not found."}), 404
+    allowed_to_change_info = [
+    {"name", "code"}
+    ]
+    for key, value in data.items():
+        if key in allowed_to_change_info:
+            city[key] = value
+            city["updated_at"] = datetime.now()
+    for info in allowed_to_change_info:
+        if info not in data:
+            return jsonify({"message": "Missing information."}), 400
+
+@app.route('/cities/{city_id}')
+def delete_city(city_id):
+    """Delete a city."""
+    city = next((_city for _city in cities if _city["id"] == city_id), None)
+    if city is None:
+        return jsonify({"error": "City not found."}), 404
+    cities.remove(city)
+    return jsonify({"message": "City deleted with sucess."}), 200
