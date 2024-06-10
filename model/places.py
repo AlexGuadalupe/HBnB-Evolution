a23e0dc42d52f052f178415f6c7c1b4ac6744aff
Emmanuel <3ma195@gmail.com>
2024-06-10 11:53:29 -0400

    fixing model/places.py

--- model/places.py
+++ model/places.py
@@ -4,13 +4,22 @@ from datetime import datetime
 class Places:
     places_count = 0
 
-    def __init__(self, name, description, location, user_id):
+    def __init__(self, name, description, address, city_id, latitude,
+                 longitude, host_id, number_of_rooms, number_of_bathrooms,
+                 price_per_night,max_guests, amenities_ids):
         self.places_id = uuid.uuid4()
         self.name = name
         self.description = description
-        self.location = location
-        self.user_id = user_id
-        self.amenities = []
+        self.address = address
+        self.city_id = city_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.host_id = host_id
+        self.number_of_rooms = number_of_rooms
+        self.number_of_bathrooms = number_of_bathrooms
+        self.price_per_night = price_per_night
+        self.max_guests = max_guests
+        self.amenities_ids = []
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
